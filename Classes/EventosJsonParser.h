164d383a13a82ab6246e6de91dc2536db0507022
Fernando Bass <ferbass@gmail.com>
2011-05-23 10:02:37 -0300

    classe para parser dos eventos

---
+++ Classes/EventosJsonParser.h
@@ -0,0 +1,16 @@
+//
+//  EventosJsonParser.h
+//  Agendatech
+//
+//  Created by Fernando Ribeiro on 5/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface EventosJsonParser : NSObject {
+
+}
+
+@end
---
+++ Classes/EventosJsonParser.m
@@ -0,0 +1,14 @@
+//
+//  EventosJsonParser.m
+//  Agendatech
+//
+//  Created by Fernando Ribeiro on 5/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "EventosJsonParser.h"
+
+
+@implementation EventosJsonParser
+
+@end
