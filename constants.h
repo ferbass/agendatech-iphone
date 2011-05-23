e32f376f243bbabd8c5ad34810768b26ff3697d1
Fernando Bass <ferbass@gmail.com>
2011-05-23 20:25:06 -0300

    url para grupos

--- constants.h
+++ constants.h
@@ -1,2 +1,3 @@
 #define EVENTOS_BASE_URL @"http://www.agendatech.com.br/mobile/eventos.json"
+#define URL_PARA_GRUPOS @"http://www.agendatech.com.br/mobile/grupos.json"
 #define URL_PARA_IMAGENS @"http://s3.amazonaws.com/agendatech_logos/thumb/%@"
